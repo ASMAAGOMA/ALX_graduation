ddbdd49db1accd72ab6499eda744e02ea4d730e0
asmaagoma <asmaagadallaah@gmail.com>
2024-09-17 10:06:24 +0300

    deploying

--- back-end/config/corsOptions.js
+++ back-end/config/corsOptions.js
@@ -1,15 +1,17 @@
 const allowedOrigins = require('./allowedorigin')
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) != -1 || !origin) {
-            callback(null, true)
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
         } else {
-            callback(new Error('Not allowed by CORS'))
+            callback(new Error('Not allowed by CORS'));
         }
     },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
     optionsSuccessStatus: 200
-}
+};
 
-module.exports = corsOptions
+module.exports = corsOptions;
